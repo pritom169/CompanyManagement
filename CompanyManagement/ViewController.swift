f6edc4ecfbdbe6a91743ed0fb1cdf55c240c7221
Pritom Mazumder <pritom.mozumder@northsouth.edu>
2019-03-04 16:23:41 +0600

    Initial Commit

---
+++ CompanyManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompanyManagement
+//
+//  Created by bs137 on 3/4/19.
+//  Copyright © 2019 Pritom Mazumder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
