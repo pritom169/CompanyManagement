250f57fab581ff8a861f8c761f9c83d3c68e17c9
Pritom Mazumder <pritom.mozumder@northsouth.edu>
2019-03-04 17:11:22 +0600

    Lesson-1

--- CompanyManagement/ViewController.swift
+++ CompanyManagement/ViewController.swift
@@ -12,9 +12,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
+        
+        navigationItem.title = "Companies"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
+        
+        setupNavigationStyle()
+        
     }
-
-
+    
+    @objc fileprivate func handleAddCompany(){
+        print("Adding new company!")
+    }
+    
+    fileprivate func setupNavigationStyle(){
+        navigationController?.navigationBar.isTranslucent = false
+        
+        let lightRed = UIColor(displayP3Red: 247/255, green: 66/255, blue: 82/255, alpha: 1)
+        navigationController?.navigationBar.barTintColor = lightRed
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+    }
 }
 
